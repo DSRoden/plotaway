1a89b4defa08f7f90fe1120e60aaac1976275dc7
DSRoden <Daniel.S.Roden@gmail.com>
2014-09-23 23:57:23 -0500

    Early implementation of angular-gridster on dashboard

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
 (function(){
   'use strict';
 
-  angular.module('mean-template', ['ngRoute', 'LocalForageModule'])
+  angular.module('plotaway', ['gridster','ngRoute', 'LocalForageModule', 'ui.bootstrap', 'ngAnimate'])
   .config(['$routeProvider', '$httpProvider', '$localForageProvider', function($routeProvider, $httpProvider, $localForageProvider){
     $routeProvider
     .when('/', {templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
     .when('/register', {templateUrl:'/views/register/register.html', controller:'RegisterCtrl'})
     .when('/login',    {templateUrl:'/views/login/login.html',       controller:'LoginCtrl'})
     .when('/logout',   {templateUrl:'/views/logout/logout.html',     controller:'LogoutCtrl'})
+    .when('/dashboard',   {templateUrl:'/views/dashboard/view.html',     controller:'DashboardCtrl'})
     .otherwise({redirectTo:'/'});
 
     $httpProvider.interceptors.push('HttpInterceptor');
